41366900095d9c459ffd2fe972c6de0af4d3166d
jake0627a1 <jake0627a1@gmail.com>
2023-05-03 23:08:10 +0800

    update

--- nft-boneboss/src/index.js
+++ nft-boneboss/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/reset.css'; //antd ignore war
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
